0309fe9730a7403dc5c266fc630edffadc9b3c5b
P-AdityaPandey98 <adityapandeymfsd@gmail.com>
2026-04-22 11:21:00 +0530

    Updated

---
+++ NotepadCode/Collection.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collection{
+
+public static void main(String[] args){
+
+
+ArrayList<Integer> al = new ArrayList<>(List.of(1,2,3,4,5,6));
+System.out.println(al);
+
+
+}
+
+}
---
+++ NotepadCode/FindingLength.java
@@ -0,0 +1,15 @@
+public class FindingLength {
+ 
+
+    int[] arr ={1,2,3,4,5,6,7,8};
+
+    void traverse(){
+        for (int i : arr){
+            System.out.println("arr["+i+"] : " arr[i]);
+
+        }
+public static void main(String[] args){
+    }
+
+
+}
